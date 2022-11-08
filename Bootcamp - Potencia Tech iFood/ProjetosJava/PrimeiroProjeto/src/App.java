4b2b43633ec2247f798812bdff2e3106ab7ee039
joeyOjuara <tca.thiagocostaaraujo@gmail.com>
2022-11-08 13:59:27 -0300

    Acrescentar "como fazer comentários"

--- Bootcamp - Potencia Tech iFood/ProjetosJava/PrimeiroProjeto/src/App.java
+++ Bootcamp - Potencia Tech iFood/ProjetosJava/PrimeiroProjeto/src/App.java
@@ -1,5 +1,19 @@
 public class App {
+
+    /**
+     * Esse comentário é para documentar um classe ou método que está por vir.
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // Comentário de apenas uma linha
+
+        /* Comentário
+         * em
+         * várias
+         * linhas
+         */
     }
 }
